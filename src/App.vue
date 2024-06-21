f5cb914705d775678bc23795b4af898e71c15d1b
Melih Can <melihcankilic@gmail.com>
2024-06-21 12:23:45 +0300

    qwe

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,6 @@ watch(
 
 <template>
   <component :is="layout" />
-  <div class="header">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/login">Logout</RouterLink>
-    </nav>
-  </div>
-  <RouterView />
 </template>
 
 <style scoped></style>
